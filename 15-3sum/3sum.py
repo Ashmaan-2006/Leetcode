14eda35b80c629a4456fe0cb7934a580f8e41db7
Ashmaan <89040615+Ashmaan-2006@users.noreply.github.com>
2026-05-05 16:07:45 -0400

    Time: 751 ms (65.62%) | Memory: 18.4 MB (51.24%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,24 +1,32 @@
 class Solution(object):
     def threeSum(self, nums):
-        result = set()
+        nums.sort()
+        result = []
 
         for i in range(len(nums)):
-            seen = set()
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-            for j in range(i + 1, len(nums)):
+            left = i + 1
+            right = len(nums) - 1
 
-                num1 = nums[i]
-                num2 = nums[j]
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
 
-                third = -(num2 + num1)
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
 
-                if third in seen:
-                    result.add(tuple(sorted([num1, num2, third])))
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
 
-                seen.add(num2)
-                    
-        return [list(triplet) for triplet in result]
-                    
+                    left += 1
+                    right -= 1
 
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
 
-        
+        return result
