8682cb968c3b951cef194cd77948e2e4873c8509
Ashmaan <89040615+Ashmaan-2006@users.noreply.github.com>
2026-07-30 20:03:31 -0400

    Time: 7 ms (6.81%) | Memory: 12.6 MB (25.09%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.py
@@ -1,23 +1,18 @@
 class Solution(object):
     def minTimeToVisitAllPoints(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: int
-        """
 
-        sum = 0
+        counter = 0
 
         for i in range(len(points) - 1):
-
+    
             x1, y1 = points[i]
             x2, y2 = points[i + 1]
 
-            diffx = abs(x2 - x1)
-            diffy = abs(y2 - y1)
+            each_time = max( (abs(x2 - x1)), (abs(y2 - y1)) )
 
-            sum += max(diffx, diffy)
-        
-        return sum
+            counter += each_time
 
+            #assuming there are 3 points, this is would be 0, 1 ,2, need it from 0 to 1 and 1 to 2, not 2 to 3
 
+        return counter
         
